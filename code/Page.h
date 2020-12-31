2258a4624e4b4e323a77ffda259312d4a920743a
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2020-12-31 11:59:37 +0530

    Create Page.h

---
+++ code/Page.h
@@ -0,0 +1,21 @@
+const char MG[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>connect everyone</title>
+  </head>
+  <body style="background-color:black">
+    <h1 style="color:white; font-family:Comic Sans MS">Welcome</h1>
+    <hr>
+    <p style="color:lightgreen; font-family:Comic Sans MS">Control Pannel:- </p>
+    <a href="/Read1/10"><button>Switch 1 ON</button></a>
+    <a href="/Read1/11"><button>Switch 1 OFF</button></a>
+    <br>
+    <a href="/Read1/20"><button>Switch 2 ON</button></a>
+    <a href="/Read1/21"><button>Switch 2 OFF</button></a>
+    <br>
+    <hr>
+    <p style="color:orange; font-family:Comic Sans MS">Made by :- Sukarna Jana<br>Running on ESP8266</p>
+  </body>
+</html>
+)=====";
